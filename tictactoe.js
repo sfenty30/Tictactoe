5b6e8ca7fbd5d05dc4d3e112c25bd2c65faf5bec
fenty <sfenty30@gmail.com>
2024-01-05 00:53:08 +0100

    added tictactoe.html, css and javascript

---
+++ tictactoe.js
@@ -0,0 +1,25 @@
+const statusDisplay = document.querySelector('.game--status');
+let gameActive = true;
+let currentPlayer = "X";
+let gameState = ["", "", "", "", "", "", "", "", ""];
+const winningMessage = () => `Player ${currentPlayer} has won!`;
+const drawMessage = () => `Game ended in a draw!`;
+const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
+statusDisplay.innerHTML = currentPlayerTurn();
+function handleCellPlayed() {
+
+}
+function handlePlayerChange() {
+
+}
+function handleResultValidation() {
+
+}
+function handleCellClick() {
+
+}
+function handleRestartGame() {
+
+}
+document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
+document.querySelector('.game--restart').addEventListener('click', handleRestartGame);
